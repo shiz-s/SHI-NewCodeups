1968b7009cbe8764799dde60cb5581dbc146dd4e
shizuka <shizka.0713@gmail.com>
2023-06-23 05:34:25 +0900

    2023.6.23 5:33 push

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+    
+// ドロワーメニュー
+$(".js-hamburger").click(function(){
+    if($(".js-hamburger").hasClass("is-active")){
+        $(".js-hamburger").removeClass("is-active");
+        $(".js-sp-nav").removeClass("is-active");
+    } else {
+        $(".js-hamburger").addClass("is-active");
+        $(".js-sp-nav").addClass("is-active");
+    };
+});
 
 });
