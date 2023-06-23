141f012c7ee590b68d7dcc26b306b1f81fc407d5
shizuka <shizka.0713@gmail.com>
2023-06-24 05:58:24 +0900

    20230624 05:58 push

--- src/js/script.js
+++ src/js/script.js
@@ -23,56 +23,20 @@ var swiper = new Swiper(".js-mv-swiper", {
 });
 
 // campaign swiper
-// var campaignSwiper = new Swiper(".js-campaign-swiper", {
-//     spaceBetween: 20, // スライド間の距離
-//     loop: true, // ループ
-//     // centeredSlides: true, // アクティブなスライドを中央にする
-//     autoplay: {
-//         delay: 5000,
-//     },
-//     // roundLength: true,
-//     speed: 200,
-//     slidesPerView: 1.2, // 一度に表示する枚数
-//     breakpoints:{
-//         768:{
-//             slidesPerView: 3.5,
-//             loop: true, 
-//             autoplay: {
-//                 delay: 5000,
-//             },
-//         }
-//     },
-   
-// });
-
-var campaignSwiper = null;
-
-function initSwiper() {
-  if (campaignSwiper !== null) {
-    campaignSwiper.destroy(); // Swiperインスタンスを破棄
-  }
-
-  campaignSwiper = new Swiper(".js-campaign-swiper", {
-    spaceBetween: 20,
-    loop: true,
-    speed: 200,
-    slidesPerView: getSlidesPerView(),
+var campaignSwiper = new Swiper(".js-campaign-swiper", {
+    spaceBetween: 20, // スライド間の距離
+    loop: true, // ループ
+    // centeredSlides: true, // アクティブなスライドを中央にする
     autoplay: {
-      delay: 5000,
+        delay: 5000,
     },
-  });
-}
-
-function getSlidesPerView() {
-  if (window.innerWidth >= 768) {
-    return 3.5;
-  } else {
-    return 1.2;
-  }
-}
-
-initSwiper(); // 初回のSwiperインスタンス作成
-
-window.addEventListener("resize", function () {
-  initSwiper(); // ブレイクポイントでのリサイズ時にSwiperインスタンスを再作成
+    speed: 200,
+    slidesPerView: 1.2, // 一度に表示する枚数
+    
+    breakpoints:{
+        768:{
+            slidesPerView: 3.5,
+        }
+    }
 });
+
