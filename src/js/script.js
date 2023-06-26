7dc745d7459ad53e1623a7b445e6bb20ccf77be5
shizuka <shizka.0713@gmail.com>
2023-06-26 14:16:50 +0900

    20230626 14:16

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,53 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     });
 });
 
+// page top btn
+var topBtn = $("#page-top");
+// topBtn.hide();
+
+// btnの表示
+// $(window).scroll(function(){
+//     if($(this).scrollTop() > 70){
+//         topBtn.fadeIn();
+//     } else {
+//         topBtn.fadeOut();
+//     }
+// });
+
+// btnをクリックしたらスクロールして上に戻る
+topBtn.click(function () {
+    $("body,html").animate({
+        scrollTop: 0
+    }, 300, 'swing');
+    return false;
+});
+
+//要素の取得とスピードの設定
+// var box = $('.price__img-box'),
+//     speed = 700;
+
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+// box.each(function () {
+//     $(this).append('<div class="color"></div>')
+//     var color = $(this).find($('.color')),
+//         image = $(this).find('img');
+//     var counter = 0;
+
+//     image.css('opacity', '0');
+//     color.css('width', '0%');
+    //inviewを使って背景色が画面に現れたら処理をする
+//     color.on('inviewenter', function () {
+//         if (counter == 0) {
+//             $(this).delay(200).animate({ 'width': '100%' }, speed, function () {
+//                 image.css('opacity', '1');
+//                 $(this).css({ 'left': '0', 'right': 'auto' });
+//                 $(this).animate({ 'width': '0%' }, speed);
+//             })
+//             counter = 1;
+//         }
+//     });
+// });
+
 // mv swiper
 var swiper = new Swiper(".js-mv-swiper", {
     autoplay: {
@@ -35,10 +82,10 @@ var campaignSwiper = new Swiper(".js-campaign-swiper", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-    
-    breakpoints:{
-        768:{
+    },
+
+    breakpoints: {
+        768: {
             slidesPerView: 3.5,
         }
     }
